df8b6db35e066c7a74a3a72d2c6fcca6be753177
Aurora Vaughn <avaughn.git@gmail.com>
2018-02-05 16:31:06 -0600

    working on albums showing under artist

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,57 +1,34 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
 import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import { HashRouter, Route } from 'react-router-dom';
+import AllArtists from './AllArtists.jsx';
+import SingleArtist from './SingleArtist.jsx'
 
 export default class Main extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.state = {
-      albums: [],
-      selectedAlbum: {}
-    };
-    this.selectAlbum = this.selectAlbum.bind(this);
-    this.deselectAlbum = this.deselectAlbum.bind(this);
   }
-
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
-
-  selectAlbum (albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
-  }
-
-  deselectAlbum () {
-    this.setState({ selectedAlbum: {}});
-  }
-
-  render () {
+  render() {
     return (
-      <div id="main" className="container-fluid">
-        <div className="col-xs-2">
-          <Sidebar deselectAlbum={this.deselectAlbum} />
+      <HashRouter>
+        <div id="main" className="container-fluid">
+          <div className="col-xs-2">
+            <Sidebar />
+          </div>
+          <div className="col-xs-10">
+            <Route exact path="/" component={AllAlbums} />
+            <Route exact path="/albums" component={AllAlbums} />
+            <Route path="/albums/:albumId" component={SingleAlbum} />
+            <Route exact path="/artists" component={AllArtists} />
+            <Route exact path="/artists/:artistId" component={SingleArtist} />
+          </div>
+          <Player />
         </div>
-        <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
-        <Player />
-      </div>
+      </HashRouter>
     );
   }
 }
--- browser/react/components/SingleAlbum.js
+++ browser/react/components/SingleAlbum.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import Songs from '../components/Songs';
+import axios from 'axios'
+
 
 export default class SingleAlbum extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedAlbum: {}
+    }
+  }
 
-  render () {
-
-    const album = this.props.album;
+  componentDidMount() {
+    const albumId = this.props.match.params.albumId;
+    axios.get(`/api/albums/${albumId}`)
+      .then(res => res.data)
+      .then(selectedAlbum => {
+        this.setState({ selectedAlbum })
+      });
+  }
+  render() {
+    const album = this.state.selectedAlbum;
 
     return (
       <div className="album">
         <div>
-          <h3>{ album.name }</h3>
-          <img src={ album.imageUrl } className="img-thumbnail" />
+          <h3>{album.name}</h3>
+          <img src={album.imageUrl} className="img-thumbnail" />
         </div>
         <Songs songs={album.songs} />
       </div>
